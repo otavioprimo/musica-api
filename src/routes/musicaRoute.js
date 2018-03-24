07e22a8e5f815a63d579f131a03ff914e997c259
Otavio Primo <otavioprimo@hotmail.com>
2018-03-24 17:37:25 -0300

    sss

--- src/routes/musicaRoute.js
+++ src/routes/musicaRoute.js
@@ -3,11 +3,11 @@ const express = require('express'),
 
 var musicController = require('../controllers/v1/musicController');
 
-router.route('/')
-    .post(musicController.cadastrarMusica)
-    .get(musicController.buscarMusicas);
+// router.route('/')
+//     .post(musicController.cadastrarMusica)
+//     .get(musicController.buscarMusicas);
 
-router.route('/search')
-    .get(musicController.buscarMusicasFiltro);
+// router.route('/search')
+//     .get(musicController.buscarMusicasFiltro);
 
 module.exports = router;
